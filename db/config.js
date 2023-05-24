3d77a5960bcfc496c4d65170b8df31973374284e
Qaswar664 <99461247+Qaswar664@users.noreply.github.com>
2023-05-24 19:37:26 +0500

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/e-commerce')
+mongoose.connect('mongodb+srv://qaswer:qaswer@cluster1.wsyee9d.mongodb.net/e-commerce')
